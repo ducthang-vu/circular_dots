e066e0d83f094e84f833c63675f66f59b565685d
Duc Thang Vu <vuducthang91@gmail.com>
2020-05-10 17:50:24 +0200

    main task completed

--- script/main.js
+++ script/main.js
@@ -21,85 +21,127 @@ class circleDot {
         this.color = color
         this.angle = Math.random() * 2 * Math.PI 
         this.arcAngle = 0.05
+        this.arcPoint = {
+            x: this.x + this.radius * Math.cos(this.angle),
+            y: this.y + this.radius * Math.sin(this.angle)
+        }
+        this.width = 2// [2, 2.5, 3, 3.5, 4][Math.random() * 5 | 0]
     }
 
-    move() {
-        this.angle += this.arcAngle
+    makePath() {
+        let start = {
+            x: this.arcPoint.x,
+            y:  this.arcPoint.y
+        }
+        let end = {
+            x: this.x + this.radius * Math.cos(this.angle + this.arcAngle),
+            y: this.y + this.radius * Math.sin(this.angle + this.arcAngle)
+        }
+
+        this.angle += this.arcAngle;
+        [this.arcPoint.x, this.arcPoint.y] = [end.x, end.y]        
+
+        return {
+            start: start,
+            end: end
+        }
     }
 
-    print() {
+    print(object) {
         c.beginPath()
         c.strokeStyle = this.color
-        c.arc(this.x, this.y, this.radius, this.angle, this.angle + this.arcAngle)
-        c.lineWidth = 2
+        c.moveTo(object.start.x, object.start.y)
+        c.lineTo(object.end.x, object.end.y)
+        c.lineWidth = this.width
         c.stroke()
     }
 }
 
 class Animation {
-    constructor(dotsNumber, translateSpeed) {
+    constructor(dotsNumber, maxTranslateSpeed) {
         self = this
         this.dotsNumber = dotsNumber
-        this.translateSpeed = translateSpeed
+        this.translateSpeed = maxTranslateSpeed
         this.dots = []
-        this.center = [canvas.width / 2, canvas.height / 2]
-
-        window.onresize = resizeCanvas
-        for (let i = 0; i < dotsNumber; i++) {
-            this.dots.push(new circleDot(canvas.width / 2, canvas.height / 2, Math.random() * 20 + 60, 'blue') )
+        this.center = {
+            x: canvas.width / 2, 
+            y: canvas.height / 2
         }
 
-        /*setInterval(() => {
-            console.log(this.center)
-        }, 1000);*/
+        for (let i = 0; i < dotsNumber; i++) {
+            let color = ['#1C3FFF','#FF0AC3','#00FFE3','#FFCC26', '#3DFF0D'][Math.random() * 5 | 0]
+            let ran = [80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 100, 102, 104, 106, 108, 110, 112, 114, 116, 118, 120, 122, 124]
+            this.dots.push(
+                new circleDot(canvas.width / 2, canvas.height / 2, ran[Math.random()*ran.length | 0], color)
+            )
+        }
     }
 
     setCenter() {
-        document.addEventListener('mousemove', function(e) {self.center = [e.clientX, e.clientY]})
-        document.addEventListener('mouseleave', () => {this.center = [canvas.width / 2, canvas.height / 2]})
+        document.addEventListener('mousemove', function(e) {
+            [self.center.x, self.center.y] = [e.clientX, e.clientY]
+        })
+        document.addEventListener('mouseleave', () => {
+            [this.center.x, this.center.y] = [canvas.width / 2, canvas.height / 2]
+        })
     }
 
-    setMousecenter() {
+    attractToCenter() {
         for (let dot of this.dots) {
-            let distanceX = (this.center[0] - dot.x)
-            let distanceY = (this.center[1] - dot.y)
-            //console.log(distanceY)
+            let distanceX = (this.center.x - dot.x)
+            let distanceY = (this.center.y - dot.y)
             let distance = ((distanceX**2 + distanceY**2)**0.5)
             
-            if (distance > 0 && distance < 0.5) {
-                dot.x = this.center[0]
-                dot.y = this.center[1]
-            } else if (distance > 0.5) {
+            let step = this.translateSpeed
+            if (step > distance) {
+                step = this.translateSpeed / 25
+            }
+
+            if (distance) {
+                let ratio = step / distance
+                let movX = distanceX * ratio
+                let movY = distanceY * ratio
+
+                if (Math.abs(movX) > Math.abs(distanceX)) dot.x = this.center.x
+                else dot.x += distanceX * ratio
+
+                if (Math.abs(movY) > Math.abs(distanceY)) dot.y = this.center.y
+                else dot.y += distanceY * ratio
+            }
+            
+
+            /*
+            if (distance > 0 && distance <= this.MaxTranslateSpeed) {
+                dot.x = this.center.x
+                dot.y = this.center.y
+            } else if (distance > this.translateSpeed) {
                 let ratio = this.translateSpeed / distance
-                //console.log(ratio)
-                //console.log(distanceX, ratio, distanceX * ratio)
                 dot.x += distanceX * ratio
                 dot.y += distanceY * ratio
             }
-
+            */
         }
     }
 
     addCanvasLayer() {
         c.beginPath()
         c.rect(0, 0, canvas.width, canvas.height)
-        c.fillStyle = 'rgba(255, 255, 255, 0.1)'
+        c.fillStyle = 'rgba(0, 0, 0, 0.015)'
         c.fill()
     }
 
     animate() {
-        requestAnimationFrame(this.animate.bind(this));
+        requestAnimationFrame(this.animate.bind(this))
 
-        this.setMousecenter() 
         this.addCanvasLayer()
-
+        this.attractToCenter()
         for (let dot of this.dots) {
-            dot.move()
-            dot.print()
+            dot.print(dot.makePath())
         }
     }
 
     start() {
+        window.onresize = resizeCanvas
         this.setCenter()
         this.animate()
     }
